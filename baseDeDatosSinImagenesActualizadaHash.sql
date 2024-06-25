0b3c9cb6e3c593123f0af39cca4b27bb6216f85b
alegom05 <146905500+alegom05@users.noreply.github.com>
2024-06-25 12:07:15 -0500

    sql1

--- baseDeDatosSinImagenesActualizadaHash.sql
+++ baseDeDatosSinImagenesActualizadaHash.sql
@@ -19,10 +19,6 @@
 -- Table structure for table `credenciales`
 --
 
--- Crear Base de datos u.u
-CREATE DATABASE IF NOT EXISTS basededatos3;
-USE basededatos3;
-
 DROP TABLE IF EXISTS `credenciales`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
