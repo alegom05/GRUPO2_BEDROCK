c4fdc2a9cb1dcaaeb3cd80ff6ea05e0b2f9a97e2
alegom05 <146905500+alegom05@users.noreply.github.com>
2024-07-11 23:14:38 -0500

    SerenazgoServlet comentado

--- CRUD-BEDROCK/src/main/java/com/example/grupo2/Servlet/ServletsPorRolDespuesLoUsamos/SerenazgoRolServlet.java
+++ CRUD-BEDROCK/src/main/java/com/example/grupo2/Servlet/ServletsPorRolDespuesLoUsamos/SerenazgoRolServlet.java
@@ -23,7 +23,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 @MultipartConfig
-@WebServlet(name = "Seren", value = "/Seren")
+@WebServlet(name = "Sereno", value = "/Sereno")
 public class SerenazgoRolServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -56,7 +56,7 @@ public class SerenazgoRolServlet extends HttpServlet {
             //Pestaña Actualizar Información ***
             case "actualizarS":
                 String id = request.getParameter("id");
-                if(usuarioDao.listarPorId(Integer.parseInt(id)) != null){
+                if (usuarioDao.listarPorId(Integer.parseInt(id)) != null) {
                     Usuario usuario = usuarioDao.listarPorId(Integer.parseInt(id));
                     if (usuario != null) {
                         request.setAttribute("usuarioSerenazgo", usuario);
@@ -66,7 +66,7 @@ public class SerenazgoRolServlet extends HttpServlet {
                     } else {
                         response.sendRedirect("error.jsp"); // Página de error en caso de que no se encuentre la incidencia
                     }
-                }else{
+                } else {
                     response.sendRedirect("error.jsp");
                 }
 
@@ -81,196 +81,89 @@ public class SerenazgoRolServlet extends HttpServlet {
                 view = request.getRequestDispatcher("/SerenazgoJSPS/tablaIncidencias-Serenazgo.jsp");
                 view.forward(request, response);
                 break;
+            //Vista Detallar Incidencia
+            case "detallar":
+                String id2 = request.getParameter("id");
+                if (incidenciaDao.obtenerIncidenciaPorId(Integer.parseInt(id2)) != null) {
+                    Incidencia incidencia = incidenciaDao.obtenerIncidenciaPorId(Integer.parseInt(id2));
+                    if (incidencia != null) {
+                        request.setAttribute("incidencia", incidencia);
 
+                        String estado = incidencia.getEstadoIncidencia();
+                        request.setAttribute("mostrarEvaluar", "Nueva".equals(estado));
+                        request.setAttribute("mostrarVerEvaluacion", "En proceso".equals(estado));
+                        request.setAttribute("mostrarFalsaAlarma", "Nueva".equals(estado) || "En proceso".equals(estado));
+                        request.setAttribute("mostrarCerrar", "En proceso".equals(estado));
 
-            //Este case será para el listado de eventos de coordinadora
-            case "listaEventos":
-                ArrayList<Evento> listaEventos2 = eventoDao.listarEventosParaCoordi();
-                request.setAttribute("listaEventos",listaEventos2);
+                        /*view = request.getRequestDispatcher("/SerenazgoJSPS/detallesIncidencia-Serenazgo.jsp");
+                        request.getSession().setAttribute("incidenciaParaExportar", incidencia);
 
-                view =request.getRequestDispatcher("/CoordinadorasJSPS/HistorialDeEventosNew.jsp");
-                view.forward(request,response);
-                break;
-
-            //Case para crear eventos de coordinadoras
-            case "formCrearEventos":
-                view = request.getRequestDispatcher("/CoordinadorasJSPS/CrearEvento.jsp");
-                view.forward(request, response);
-                break;
-
-            //Case para editar eventos de coordinadoras
-            case "editarEventos":
-                String id2= request.getParameter("id");
-                Evento evento = eventoDao.obtenerEventoPorId(id2);
-                if (evento != null) {
-                    request.setAttribute("evento", evento);
-                    request.getRequestDispatcher("/CoordinadorasJSPS/ModificarEvento.jsp").forward(request,response);
-
-                } else {
-                    response.sendRedirect(request.getContextPath() + "/EventoServlet");
-                }
-                break;
-
-            //Case para elminar eventos
-            case "deleteEventos":
-                String ide= request.getParameter("id");
-                Evento eventoo= eventoDao.obtenerEventoPorId(ide);
-                if(eventoo!=null){
-                    try{
-                        eventoDao.borrarEvento(ide);
-                    }catch (SQLException e){
-                        System.out.println("Log:exception"+e.getMessage());
-
-                    }
-                }
-                response.sendRedirect(request.getContextPath()+"/EventoServlet");
-                break;
-
-            //Este caso servirá para cuando la coordinadora desea revisar los detalles de alguno de sus eventos creados
-            case "detallarParaCoordi":
-                String idCoordi = request.getParameter("id");
-                if(eventoDao.obtenerEventoPorId(idCoordi) != null){
-                    Evento evento2 = eventoDao.obtenerEventoPorId(idCoordi);
-                    if (evento2 != null) {
-                        request.setAttribute("evento", evento2);
-
-                        String estado = evento2.getEstadoEvento();
-                        request.setAttribute("mostrarGuardar","Pronto".equals(estado));
-                        request.setAttribute("mostrarIniciar", "Pronto".equals(estado));
-                        request.setAttribute("mostrarCerrar","En curso".equals(estado));
-
-
-                        view = request.getRequestDispatcher("/CoordinadorasJSPS/detallesEvento.jsp");
-                        view.forward(request, response);
+                        view.forward(request, response);*/
                     } else {
                         response.sendRedirect("error.jsp"); // Página de error en caso de que no se encuentre la incidencia
                     }
-                }else{
+                } else {
                     response.sendRedirect("error.jsp");
                 }
 
-                view = request.getRequestDispatcher("/HistorialDeEventosNew.jsp");
+                view = request.getRequestDispatcher("/SerenazgoJSPS/tablaIncidencias-Serenazgo.jsp");
                 view.forward(request, response);
                 break;
-
-            //Este case será para el listado de incidencias de coordinadora
-            /*case "listaIncidencias":
-
-                ArrayList<Incidencia> listaIncidencias2 = IncidenciaDao.listarIncidencias();
-                request.setAttribute("lista2", listaIncidencias2);
-
-                RequestDispatcher view2 = request.getRequestDispatcher("/CoordinadorasJSPS/ListaDeIncidencias.jsp");
-                view2.forward(request, response);
-                break;*/
-
-            case "detallarIncidencia":
-                String id3 = request.getParameter("id");
-                int idint = Integer.parseInt(id3);
-                if (incidenciaDao.obtenerIncidenciaPorId(Integer.parseInt(id3)) != null) {
-                    Incidencia incidencia = incidenciaDao.obtenerIncidenciaPorId(idint);
-                    request.setAttribute("incidencia", incidencia);
-                    RequestDispatcher view1 = request.getRequestDispatcher("/CoordinadorasJSPS/detallesIncidencia.jsp");
-                    view1.forward(request, response);
-                }else {
-                    System.out.println("Incidencia no encontrada para el ID: " + id3); // Log de incidencia no encontrada
-                    //response.sendRedirect("error.jsp");
-                }
-                break;
-
-            case "formCrearIncidencia":
-                view = request.getRequestDispatcher("/CoordinadorasJSPS/ReportarIncidenciaVerisonPRUEBA.jsp");
-                view.forward(request, response);
-                break;
-
-            //Para que la coordinadora visualice sus incidencias
-            case "listaCoordi":
-                String idCoordi2 = request.getParameter("idUsuario");
-                ArrayList<Incidencia> listaIncidenciasCoordi = incidenciaDao.listarIncidenciasDeUnUsuario(idCoordi2);
-                request.setAttribute("incidencia", listaIncidenciasCoordi);
-
-                view = request.getRequestDispatcher("/CoordinadorasJSPS/ListaDeIncidencias.jsp");
-                view.forward(request, response);
-                break;
-
-            //Pestaña Lista de vecinos ***
-            case "listarve":
-                ArrayList<Usuario> listaUsuario = usuarioDao.listarUsuarios();
-                request.setAttribute("lista",listaUsuario);
-                view =request.getRequestDispatcher("/CoordinadorasJSPS/VecinoSanmi.jsp");
-                view.forward(request,response);
-                break;
-            //Pestaña Reportar incidencia ***
-            /*case "formCrearInci":
-                view = request.getRequestDispatcher("/CoordinadorasJSPS/ReportarIncidenciaVerisonPRUEBA.jsp");
-                view.forward(request, response);
-                break;*/
-            //Pestaña Lista de incidencias -> Listar Coordis ***
-            /*case "listaCoordi":
-                String idCoordi = request.getParameter("idUsuario");
-                ArrayList<Incidencia> listaIncidenciasCoordi = incidenciaDao.listarIncidenciasDeUnUsuario(idCoordi);
-                request.setAttribute("incidencia", listaIncidenciasCoordi);
-
-                view = request.getRequestDispatcher("/CoordinadorasJSPS/ListaDeIncidencias.jsp");
-                view.forward(request, response);
-                break;*/
-            //Detalle de incidencia -> Detalles de una incidencia para Coordis
-            /*case "detallar3":
-                String id3 = request.getParameter("id");
-                int idint = Integer.parseInt(id3);
-                if (incidenciaDao.obtenerIncidenciaPorId(Integer.parseInt(id3)) != null) {
-                    Incidencia incidencia3 = incidenciaDao.obtenerIncidenciaPorId(idint);
-                    request.setAttribute("incidencia", incidencia3);
-                    RequestDispatcher view1 = request.getRequestDispatcher("/CoordinadorasJSPS/detallesIncidencia.jsp");
-                    view1.forward(request, response);
-                }else {
-                    System.out.println("Incidencia no encontrada para el ID: " + id3); // Log de incidencia no encontrada
-                    //response.sendRedirect("error.jsp");
-                }
-                break;*/
-            //Pestaña Página Principal ***
-            /*case "paginaPrincipal":
-                response.sendRedirect(request.getContextPath() + "/CoordinadorasJSPS/PaginaPrincipal.jsp");
-                break;*/
-            /*case "reportarIncidencia":
-                response.sendRedirect(request.getContextPath() + "/CoordinadorasJSPS/ReportarIncidencia.jsp");
-                break;*/
-            /*case "listarIncidencias":
-                response.sendRedirect(request.getContextPath() + "/CoordinadorasJSPS/ListaDeIncidencias.jsp");
-                break;*/
-            //Pestaña Mira tu calendario ***
-            case "calendario":
-                response.sendRedirect(request.getContextPath() + "/CoordinadorasJSPS/Calendario.jsp");
-                break;
-
-            //Pestaña Historial de Eventos ***
-            /*case "historialEventos":
-                ArrayList<Evento> listaEventos = eventoDao.listarEventosCoordi2();
-                request.setAttribute("listaEventos", listaEventos);
-                view = request.getRequestDispatcher("/CoordinadorasJSPS/HistorialDeEventosNew.jsp");
-                view.forward(request, response);
-                break;*/
-            //Vista Crear evento
-            /*case "crearev":
-                RequestDispatcher view22 = request.getRequestDispatcher("/CoordinadorasJSPS/CrearEvento.jsp");
-                view22.forward(request, response);
-                break;*/
-            /*case "detallarev":
-                String id4 = request.getParameter("id");
-                int idint4 = Integer.parseInt(id4);
-                if (incidenciaDao.obtenerIncidenciaPorId(Integer.parseInt(id4)) != null) {
-                    Evento evento = eventoDao.obtenerEventoPorId(id4);
-                    request.setAttribute("evento", evento);
-                    RequestDispatcher view1 = request.getRequestDispatcher("/CoordinadorasJSPS/detallesEvento.jsp");
-                    view1.forward(request, response);
-                }else {
-                    System.out.println("Incidencia no encontrada para el ID: " + id4); // Log de incidencia no encontrada
-                    //response.sendRedirect("error.jsp");
-                }
-                break;*/
         }
-
 
     }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
 
+        String action = request.getParameter("action") == null ? "lista" : request.getParameter("action");
+        IncidenciaDao incidenciaDao = new IncidenciaDao();
+        RequestDispatcher view;
+
+        switch (action) {
+            //Algo parecido a esto
+            case "crear":
+                String nombreIncidencia = request.getParameter("nombreIncidencia");
+                String lugar = request.getParameter("lugarIncidencia");
+                String referencia = request.getParameter("referencia");
+                String descripcionIncidencia = request.getParameter("descripcion");
+                String phoneNumber = request.getParameter("contacto");
+                boolean ambulancia = Boolean.parseBoolean(request.getParameter("ambulancia"));
+
+                Part filePart = request.getPart("imagen"); // Obtén la parte del archivo
+                InputStream foto = null;
+                if (filePart != null && filePart.getSize() > 0) {
+                    foto = filePart.getInputStream(); // Lee el contenido del archivo como un InputStream
+                }
+
+                String tipoIncidencia = request.getParameter("tipo");
+                System.out.println(tipoIncidencia);
+                int idUsuario = Integer.parseInt(request.getParameter("idUsuario"));
+
+                Incidencia nuevaIncidencia = new Incidencia();
+
+                nuevaIncidencia.setNombreIncidencia(nombreIncidencia);
+                nuevaIncidencia.setLugar(lugar);
+                nuevaIncidencia.setReferencia(referencia);
+                nuevaIncidencia.setDescripcion(descripcionIncidencia);
+                nuevaIncidencia.setContacto(phoneNumber);
+                nuevaIncidencia.setRequiereAmbulancia(ambulancia);
+                nuevaIncidencia.setFotoIncidencia(foto);
+                nuevaIncidencia.setIdTipoIncidencia(tipoIncidencia);
+                nuevaIncidencia.setIdUsuario(idUsuario);
+
+                incidenciaDao.crearIncidencia(nuevaIncidencia);
+                System.out.println(nuevaIncidencia.getNombreIncidencia());
+                System.out.println(nuevaIncidencia.getLugar());
+                System.out.println(nuevaIncidencia.getReferencia());
+                System.out.println(nuevaIncidencia.getDescripcion());
+                System.out.println(nuevaIncidencia.getContacto());
+                System.out.println(nuevaIncidencia.getIdTipoIncidencia());
+                System.out.println(nuevaIncidencia.getIdUsuario());
+                /*response.sendRedirect(request.getContextPath() + "/IncidenciaServlet?action=lista3&idUsuario=" + idUsuario);*/
+
+                break;
+
+        }
+    }
 }
